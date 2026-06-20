dee073fd03868bc88511705b438bc8f965700a96
Abhishek Gola <abhishekgola76688@gmail.com>
2026-06-20 20:02:23 +0530

    feat: add auth routes and user registration controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import authRouter from './routes/auth.routes.js';
 
 const app = express();
 
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));              // Morgan is an HTTP request logger middleware for Express applications. It logs details about incoming requests such as: HTTP method (GET, POST, etc.), URL, status code, and response time.
 
+
+app.use("/api/auth", authRouter);
 
 
 export default app;
